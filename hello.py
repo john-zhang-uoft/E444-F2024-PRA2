06dd85b2ef52b0b0236d99934ccdab6ef4d3ce12
John Zhang <johnz.zhang@mail.utoronto.ca>
2024-09-27 14:51:30 -0400

    Added email address form and display

--- hello.py
+++ hello.py
@@ -18,6 +18,7 @@ bootstrap = Bootstrap(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your email?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -25,12 +26,19 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html',
-        form = form, name = session.get('name'))
+        form = form, 
+        name = session.get('name'),
+        email = session.get('email')
+        )
 
 @app.route('/user/<name>')
 def user(name):
