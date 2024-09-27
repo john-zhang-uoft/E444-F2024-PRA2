968448101dea2b813ed2de44be24b6164ef11c7b
John Zhang <johnz.zhang@mail.utoronto.ca>
2024-09-27 14:26:47 -0400

    Added .gitignore, env file for secret key, and NameForm class

--- hello.py
+++ hello.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+    
 @app.route('/')
 def index():
     return render_template('index.html',
