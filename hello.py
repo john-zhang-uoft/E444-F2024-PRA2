ddcc51042600acd3b7c56a8a42eefd6568c13173
John Zhang <johnz.zhang@mail.utoronto.ca>
2024-09-27 15:11:32 -0400

    Added logic for checking for U of T email address

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 from datetime import datetime
 from dotenv import load_dotenv
@@ -16,9 +16,16 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
+def validate_at_symbol(form, field):
+    if '@' not in field.data:
+        raise ValidationError(f"Please include an '@' in the email address. '{field.data}' is missing an '@'.")
+    
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = StringField('What is your email?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[
+        DataRequired(), 
+        validate_at_symbol
+        ])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
